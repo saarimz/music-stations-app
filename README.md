206532f4c6f72a00ec65be995c4908ed02468724
Saarim Zaman <saarim.z@gmail.com>
2017-11-09 01:40:09 +0000

    Initial Setup

--- README.md
+++ README.md
@@ -26,3 +26,6 @@ Things you may want to cover:
 
 Please feel free to use a different markup language if you do not plan to run
 <tt>rake doc:app</tt>.
+
+TO RUN RAILS SERVER ON CLOUD9 IDE: `$ rails s -b $IP -p $PORT`
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  
+  root 'pages#home'
+  get "about" => 'pages#about'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
