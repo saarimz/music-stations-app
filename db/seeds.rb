efab6962ebfa0fe675822157ea19cee4a6d70c60
Saarim Zaman <saarim.z@gmail.com>
2017-11-14 19:22:15 +0000

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,16 @@
       title: "Dazed Digital Mixes",
       description: "All the Dazed Mixes, all in one place.",
       url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/296562077&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"
+    },
+    {
+      title: "FADER Mixtape: April 2015",
+      description: "Bringing you the best of April 2015",
+      url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/103211648&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true" 
+    },
+    {
+      title: "NAAFI 2.0",
+      description: "The raw sounds of Mexico City's nightlife",
+      url: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/327036999&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"
     }
     ]
     
